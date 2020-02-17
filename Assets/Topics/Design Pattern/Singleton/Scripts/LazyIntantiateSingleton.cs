0e5b13e4f3517451284f683c05325521838192a4
NgHaiPocketQueries <ng-hai@pocket-queries.co.jp>
2020-02-17 19:32:20 +0900

    Add Singleton Pattern

---
+++ Assets/Topics/Design Pattern/Singleton/Scripts/LazyIntantiateSingleton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazyIntantiateSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance()
+    {
+        if(_instance == null)
+        {
+            _instance = GameObject.FindObjectOfType<T>();
+            if(_instance == null)
+            {
+                _instance = new GameObject("Instance of " + typeof(T)).AddComponent<T>();
+            }
+        }
+        return _instance;
+    }
+
+    private void Awake()
+    {
+        if(_instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Topics/Design Pattern/Singleton/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Transform _character;
+    [SerializeField] private Transform _target;
+    internal void SayHi()
+    {
+        Debug.Log("Hi! I'm at " + this.gameObject.transform.position);
+    }
+
+}
