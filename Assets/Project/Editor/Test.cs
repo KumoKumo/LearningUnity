0caa17d4fa8a41428077fc7d603a120ab36e1e94
NgHaiPocketQueries <ng-hai@pocket-queries.co.jp>
2020-02-21 12:15:06 +0900

    Try out some codes from new C# 8.0

--- Assets/Project/Editor/Test.cs
+++ Assets/Project/Editor/Test.cs
@@ -1,18 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Test
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /* 
+     * C# 8.0 not yet supported by Unity 2019
+     * Might come out in the 2020 release cycle at the earliest
+     * https://www.youtube.com/watch?v=aUbXGs7YTGo
+     */
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //public bool Isweekday(DateTime time) => time.DayOfWeek switch
+    //{
+    //    //Saturday and Sunday are not weekdays
+    //    DayOfWeek.Saturday => false, 
+    //    DayOfWeek.Sunday => false,
+    //    //Every other days are weekdays
+    //    _ => true 
+    //};
+
+    public void Swapping(int a, int b) => (a, b) = (b, a);
+    string newString = $"This is the Pi number: {Math.PI,20:F3}";
 }
