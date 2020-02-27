48cec6896bfa1faee11aa8bfc2770cadf5ce8498
NgHaiPocketQueries <ng-hai@pocket-queries.co.jp>
2020-02-27 18:39:59 +0900

    Add Pooling with PI

---
+++ Assets/Project/Topics/CollidingPI/CollisionManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    [SerializeField] private int count;
+    private Cube otherCube;
+    private Cube thisCube;
+    private GameObject wall;
+    private float timeSteps = 5000;
+
+    private void Awake()
+    {
+        thisCube = gameObject.GetComponent<Cube>();
+        count = 0;
+    }
+
+    private void Start()
+    {
+        wall = GameObject.FindGameObjectWithTag("Wall");
+        otherCube = GameObject.Find("BigCube").GetComponent<Cube>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveTheCubes();
+        
+    }
+
+    private void MoveTheCubes()
+    {
+        for(int i = 0; i< timeSteps; i++)
+        {
+            if (IsCollidedWithObject)
+            {
+                CollideWithOtherCube();
+            }
+            else if (IsCollidedWithWall)
+            {
+                CollideWithWall();
+            }
+            transform.position += thisCube.Velocity * Time.fixedDeltaTime / timeSteps;
+            otherCube.transform.position += otherCube.Velocity * Time.fixedDeltaTime / timeSteps;
+        }
+    }
+
+    private bool IsCollidedWithWall => 
+        ((transform.position.x - transform.localScale.x/2) <= (wall.transform.position.x + wall.transform.localScale.x/2));
+
+    private bool IsCollidedWithObject => 
+        ((otherCube.transform.position.x - otherCube.transform.localScale.x / 2) <= (transform.position.x + transform.localScale.x / 2));
+
+    private void CollideWithWall()
+    {
+        thisCube.Velocity = -1 * thisCube.Velocity;
+        count++;
+    }
+
+    private void CollideWithOtherCube()
+    {
+        Vector3[] newVelocities = VelocityAfterCollision(thisCube.Velocity, otherCube.Velocity, thisCube.Mass, otherCube.Mass);
+        thisCube.Velocity = newVelocities[0];
+        otherCube.Velocity = newVelocities[1];
+        count++;
+    }
+
+    // Equation for One-dimensional Newtonian Elastic Collision
+    // https://en.wikipedia.org/wiki/Elastic_collision
+    private Vector3[] VelocityAfterCollision(Vector3 u1, Vector3 u2, float m1, float m2)
+    {
+        Vector3 v1 = new Vector3();
+        Vector3 v2 = new Vector3();
+
+        v1 = (m1 - m2) / (m1 + m2) * u1 + (2 * m2) / (m1 + m2) * u2;
+        v2 = (2 * m1) / (m1 + m2) * u1 + (m2 - m1) / (m1 + m2) * u2;
+
+        return new Vector3[2] {v1,v2};
+    }
+}
---
+++ Assets/Project/Topics/CollidingPI/Cube.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField][Range(1,6)] private int digits = 1;
+    [SerializeField]private Vector3 velocity;
+
+    public Vector3 Velocity
+    {
+        get => velocity;
+        set => velocity = value;
+    }
+
+    public float Mass { get; private set; } = 100f;
+
+    private void Start()
+    {
+        SetUpMass();
+        SetUpMovement();
+    }
+
+    private void SetUpMovement()
+    {
+        if(gameObject.name == "BigCube")
+        {
+            velocity = new Vector3(-speed, 0, 0);
+        }
+        else
+        {
+            velocity = Vector3.zero;
+        }
+    }
+
+    private void SetUpMass()
+    {
+        Mass = Mathf.Pow(Mass, (float)digits - 1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log($"{gameObject.name}'s mass is {Mass}");
+        }
+    }
+
+}
